4a8ac910174aa89883183fb89cd6f6de4f53ecb7
patelhet22062004 <patelhet2264@gmail.com>
2024-08-31 02:41:43 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 3000,      // Optionally specify a port
+  },
+
 })
